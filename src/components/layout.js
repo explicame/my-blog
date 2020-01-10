d0b25e1fcf55cf8f745c482ec2a8c56fcbf8a981
Aldo Monteiro <aldomonteiro@gmail.com>
2020-01-10 14:57:41 -0300

    fixes

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,55 +4,55 @@ import { Link } from 'gatsby';
 import { rhythm, scale } from '../utils/typography';
 
 class Layout extends React.Component {
-  render() {
+  render () {
     const { location, title, children } = this.props;
     const rootPath = `${__PATH_PREFIX__}/`;
     let header;
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
+    // if (location.pathname === rootPath) {
+    header = (
+      <h1
+        style={{
+          ...scale(1.5),
+          marginTop: 0,
+          marginBottom: '2rem',
+          textAlign: 'center',
+          fontSize: '3rem'
+        }}
+      >
+        <Link
           style={{
-            ...scale(1.5),
-            marginTop: 0,
-            marginBottom: '2rem',
-            textAlign: 'center',
-            fontSize: '3rem'
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
           }}
+          to={`/`}
         >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      );
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      );
-    }
+          {title}
+        </Link>
+      </h1>
+    );
+    // } else {
+    //   header = (
+    //     <h3
+    //       style={{
+    //         fontFamily: `Montserrat, sans-serif`,
+    //         marginTop: 0,
+    //       }}
+    //     >
+    //       <Link
+    //         style={{
+    //           boxShadow: `none`,
+    //           textDecoration: `none`,
+    //           color: `inherit`,
+    //         }}
+    //         to={`/`}
+    //       >
+    //         {title}
+    //       </Link>
+    //     </h3>
+    //   );
+    // }
     return (
       <div
         style={{
